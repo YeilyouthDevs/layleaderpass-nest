6243cc162c5f74fc4f6d3b1405dab452549a3d4f
unknown <kjh36102@gmail.com>
2024-11-01 05:47:44 +0900

    개발환경에서 별도의 데이터베이스 사용하게 함, Mock SQL 작성 가능하게하고 개발모드로 시작 시 자동으로 입력되게함

---
+++ src/database-init.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, OnModuleInit } from "@nestjs/common";
+import * as path from "path";
+import * as fs from 'fs/promises';
+import { Sequelize } from "sequelize-typescript";
+
+@Injectable()
+export class DatabaseInitService implements OnModuleInit {
+    constructor(private readonly sequelize: Sequelize) {}
+
+    async onModuleInit() {
+
+        // 개발환경인 경우 mock_sql폴더 내 sql파일을 모두 읽어 비동기적으로 실행한다.
+        if (process.env.NODE_ENV === 'development') {
+            const mockDir = path.join(process.cwd(), 'mock_sql');
+
+            try {
+                const files = await fs.readdir(mockDir);
+
+                const sqlPromises = files.map(async (file) => {
+                    if (file.endsWith('.sql')) {
+                        const filePath = path.join(mockDir, file);
+                        const sql = await fs.readFile(filePath, 'utf8');
+                        return this.sequelize.query(sql);
+                    }
+                });
+
+                await Promise.all(sqlPromises);
+                console.log('Mock 데이터 생성 완료.');
+
+            } catch (error) {
+                console.error('Mock 데이터 생성 중 오류 발생:', error);
+            }
+        }
+    }
+}
